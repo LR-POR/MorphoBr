a0ebdbc496035642277788cecb156a169bae309b
Alexandre Rademaker <arademaker@gmail.com>
2018-04-17 13:49:03 -0300

    related to #20 : improve merging garcia+freeling

--- tmp/garcia+freeling.sh
+++ tmp/garcia+freeling.sh
@@ -1,4 +1,5 @@
-sbcl --load garcia+freeling.lisp --eval '(progn (main) (quit))' 
-cat freeling dicc-AO3_long-1pl.src | sort | uniq > garcia+freeling.uniq.src
-
+sbcl --load garcia+freeling.lisp --eval '(progn (main) (quit))'
+awk 'length($3) < 7 && $3 ~ /^A.*/ {print $1,$2,$3 "0"; next} {print}' dicc-AO3_long-1pl.src > dicc-adj.src
+cat freeling dicc-adj.src | sort | uniq > garcia+freeling.uniq.src
+rm dicc-AO3_long-1pl.src dicc-adj.src
 
