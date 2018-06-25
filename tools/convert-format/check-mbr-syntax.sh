54f1bd057d870a9207fa802f15250c43b7994eee
odanoburu <bcclaro@gmail.com>
2018-06-25 21:56:02 +0000

    (#53) use C runtime for better performance in GF syntax-checking

--- tools/convert-format/check-mbr-syntax.sh
+++ tools/convert-format/check-mbr-syntax.sh
@@ -6,4 +6,4 @@ BEGIN {  print "import MorphoMbr.gf" }
     gsub(/\+/, " +", $2) ; # add space before plus sign
     gsub(/\./, " . ", $2) ; # add space between clitics elements
     print "p \"" $1 " &t " $2 "\"" ; # add &t in lieu of TAB
-}' "$1" | gf --run | grep -n "The parser"
+}' "$1" | gf --crun | grep -n "The parser"
