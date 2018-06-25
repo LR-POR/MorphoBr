a4aad2636853a4855a8d7d5545fd9afc713d86e4
odanoburu <bcclaro@gmail.com>
2018-06-25 21:48:29 +0000

    add GF syntax checking code

---
+++ tools/convert-format/check-mbr-syntax.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+awk '
+BEGIN {  print "import MorphoMbr.gf" }
+{
+    gsub(/\+/, " +", $2) ; # add space before plus sign
+    gsub(/\./, " . ", $2) ; # add space between clitics elements
+    print "p \"" $1 " &t " $2 "\"" ; # add &t in lieu of TAB
+}' "$1" | gf --run | grep -n "The parser"
