e31541c53a9d927b14551c8f6cc2a3c375b27178
Ana <analuizanunes8@gmail.com>
2021-05-19 03:26:07 -0300

    check unification

--- src/CheckUnification.py
+++ src/CheckUnification.py
@@ -9,40 +9,8 @@ from conllu import parse_incr
 import sys
 import os
 
-
-morphobr_to_bosque = {
-    'A': ['Cat=ADJ'],
-    'ADV': ['Cat=ADV'],
-    'N': ['Cat=NOUN'],
-    'V': ['Cat=VERB'],
-    'F': ['Gender=Fem'],
-    'M': ['Gender=Masc'],
-    'SG': ['Number=Sing'],
-    'PL': ['Number=Plur'],
-    'NEG': ['Polarity=Neg'],
-    'SUPER': ['Degree=Abs'],
-    'DIM': ['Degree=Dim'],
-    'AUG': ['Degree=Aug'],
-    '1': ['Person=1'],
-    '2': ['Person=2'],
-    '3': ['Person=3'],
-    'INF': ['VerbForm=Inf'],
-    'GRD': ['VerbForm=Ger'],
-    'PTPST': ['VerbForm=Part','Tense=Past'],
-    'PRS': ['Mood=Ind','Tense=Pres'],
-    'IMPF': ['Mood=Ind','Tense=Imp'],
-    'PRF': ['Mood=Ind','Tense=Past'],
-    'FUT': ['Mood=Ind','Tense=Fut'],
-    'PQP': ['Mood=Ind','Tense=Pqp'],
-    'SBJR': ['Mood=Sub','Tense=Pres'],
-    'SBJP': ['Mood=Sub','Tense=Imp'],
-    'SBJF': ['Mood=Sub','Tense=Fut'],
-    'SUBJR': ['Mood=Sub','Tense=Pres'],
-    'SUBJP': ['Mood=Sub','Tense=Imp'],
-    'SUBJF': ['Mood=Sub','Tense=Fut'],
-    'IMP': ['Mood=Imp'],
-    'COND': ['Mood=Cod']     
-}
+with open('morphobr_to_bosque.json') as f:
+    morphobr_to_bosque = json.load(f)
 
 
 def check(token_fst,entries):
@@ -121,13 +89,16 @@ def readMorpho(path):
     return morpho
 
 
+# sys.argv[1]: diretório MorphoBr
+# sys.argv[2]: arquivo conllu
+
 if __name__ == "__main__":
-    morpho = readMorpho("/home/ana/dhbb/MorphoBr")
+    morpho = readMorpho(sys.argv[1])
 #    for root,_, files in os.walk("/home/ana/dhbb/dhbb-nlp/udp-mini", topdown=False):
 #        for f in files:
 #            print("Processing '%s':" % f)
 #            with open(os.path.join(root,f), "r", encoding="utf-8") as file:
-    with open("/home/ana/dhbb/dhbb-nlp/udp-mini/161.conllu") as file:
+    with open(sys.argv[2]) as file:
         for tks in conllu.parse_incr(file):
             for token in tks:
                 if token["upos"] in ["ADJ","ADV","NOUN","VERB"]:
